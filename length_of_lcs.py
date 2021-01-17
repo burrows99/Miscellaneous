1adb7fce90c8ba486176fb6e7280636e444c1867
Raunak Burrows <69508267+burrows99@users.noreply.github.com>
2021-01-18 01:42:40 +0530

    Create length_of_lcs.py

---
+++ length_of_lcs.py
@@ -0,0 +1,23 @@
+def lcs(a,b):
+    n=len(a)
+    m=len(b)
+    dp=[[-1]*(m+1) for i in range(n+1)]
+    
+    #initialization
+    dp[0][0]=0
+    for i in range(1,n+1):
+        dp[i][0]=0
+    for i in range(1,m+1):
+        dp[0][i]=0
+    
+    #dp
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            if(a[i-1]==b[j-1]):
+                dp[i][j]=1+dp[i-1][j-1]
+            else:
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+    return(dp[-1][-1])
+a='ABCDGH'
+b='AEDFHR'
+print(lcs(a,b))
